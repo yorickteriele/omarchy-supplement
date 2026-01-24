93cbdb3149d71ac54cc31c15c1559229380c0341
Yorick te Riele <yorick.teriele@outlook.com>
2026-01-24 19:20:27 +0100

    Adds removal scripts
    Adds scripts to handle the removal of applications.
    
    This change introduces a new script that iterates through a
    directory of removal scripts and executes them if they are
    executable. It also adds a script to remove the 1Password and
    1Password CLI applications.

--- apps/remove.sh
+++ apps/remove.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
 echo "Temp -> Nothing to remove"
+
+for file in apps/removes/*; do
+  if [ -x "$file" ] && [ ! -d "$file" ]; then
+    echo "Removing: $file"
+    "$file"
+  else
+    echo "Not an executable $file"
+  fi
+done
---
+++ apps/removes/1password.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Remove Beta
+yay -R --noconfirm 1password-beta
+
+# Remove CLI
+yay -R --noconfirm 1password-cli
